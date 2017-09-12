95c1691cd1c2aacab66bc9eedbbb2bee617c5010
root <asch716@gmail.com>
2017-09-12 02:05:49 +0000

    Added models, renamed files

--- MVVM.py
+++ MVVM.py
@@ -7,29 +7,42 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class User(db.Model):
+class Client(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    name = db.Column(db.String(80), unique=True)
+    feature_requests = db.relationship('FeatureRequest', backref='client')
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+class ProductArea(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    feature_requests = db.relationship('FeatureRequest', backref='product_area')
+
+class FeatureRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True)
+    description = db.Column(db.Text)
+    priority = db.Column(db.Integer)
+    due = db.Column(db.DateTime)
+    client = db.Column(db.Integer, db.ForeignKey('Client.id')) 
+    product_area = db.Column(db.Integer, db.ForeignKey('ProductArea.id'))    
+   
+    def __init__(self, title):
+        self.title = title
 
     def __repr__(self):
-        return '<User %r>' % self.username
-        
+        return '<Name %r>' % self.title
+
 def bad_request(message):
     response = jsonify({'error': message})
     response.status_code = 400
     return response
 
 @app.route('/')
-def hello_world(name=None):
+def home(name=None):
 	return render_template('feature_request.html', name=name)
 
-@app.route('/admin', methods=['GET', 'POST'])
-def print_users():
+@app.route('/feature-request', methods=['GET', 'POST'])
+def handle_request():
     if request.method == 'POST':
     	result = request.get_json()
     	try:
@@ -44,6 +57,3 @@ def print_users():
     		return bad_request(str(e))
         return render_template('result.html', result=result)
 
-    '''
-    user = User.query.filter_by(username='admin').first_or_404()
-	'''
