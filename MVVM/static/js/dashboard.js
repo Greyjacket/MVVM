84682d383a7ff97eda72ec21a4e3007a9a97c84c
root <asch716@gmail.com>
2017-09-16 00:26:49 +0000

    Updated dashboard

--- MVVM/static/js/dashboard.js
+++ MVVM/static/js/dashboard.js
@@ -1,20 +1,28 @@
 $(document).ready(function(){
 
-    function featureModel() {
-      var self = this;
-      self.title = ko.observable();
-      self.description = ko.observable();
+    function featureModel(){
+      var self = this
+      self.title = ko.observable()
+      self.description = ko.observable()
       self.client = ko.observable()
       self.priority = ko.observable()
       self.due = ko.observable()
       self.productArea = ko.observable()
+      self.mapProperties = function(data){
+        self.title(data.title)
+        self.description(data.description)
+        self.client(data.client)
+        self.due(data.due)
+        self.productArea(data.product_area)  
+      }
     }
+    
+    var app = new featureModel()
+    ko.applyBindings(app)
 
     $.get("/feature-request", function(data, status){
-        alert("Data: " + data.title + "\nStatus: " + status);
-    });
+        app.mapProperties(data)
+    })
 
-
-        
-      ko.applyBindings(new featureModel());
-    });
+    
+})
